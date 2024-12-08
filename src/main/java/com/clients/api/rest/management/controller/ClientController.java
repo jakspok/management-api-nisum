6c1e2bf6f89b13f37bfd278dc77e70143dae73d0
Juan Carlos <jakspok@gmail.com>
2024-12-08 02:42:27 -0500

    add comments in controller

--- src/main/java/com/clients/api/rest/management/controller/ClientController.java
+++ src/main/java/com/clients/api/rest/management/controller/ClientController.java
@@ -3,75 +3,83 @@ package com.clients.api.rest.management.controller;
 import com.clients.api.rest.management.model.Client;
 import com.clients.api.rest.management.model.ClientResponse;
 import com.clients.api.rest.management.services.IClientService;
-import io.vavr.control.Try;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("/client-management")
+@Tag(name = "Client Management", description = "API para gestionar clientes")
 public class ClientController {
 
-    private static final Logger LOGGER = LogManager.getLogger(ClientController.class);
-    private final IClientService iClientService;
+  private static final Logger LOGGER = LogManager.getLogger(ClientController.class);
+  private final IClientService iClientService;
 
-    @Autowired
-    public ClientController(final IClientService iClientService) {
+  @Autowired
+  public ClientController(final IClientService iClientService) {
+    this.iClientService = iClientService;
+  }
 
-        this.iClientService = iClientService;
-    }
+  @Operation(
+      summary = "Obtener cliente por ID",
+      description = "Retorna un cliente basado en su ID.")
+  @GetMapping("/client/{id}")
+  public ResponseEntity<Client> getClientById(
+      @Parameter(description = "ID del cliente", required = true) @PathVariable(value = "id")
+          String id) {
+    LOGGER.info("Init getClientById Client Id: {}", id);
+    return iClientService.findByClientId(id);
+  }
 
-    @GetMapping("/client/{id}")
-    public ResponseEntity<Client> getClientById(@PathVariable(value = "id") String id) {
+  @Operation(summary = "Crear cliente", description = "Registra un nuevo cliente en el sistema.")
+  @PostMapping("/client")
+  public ResponseEntity<ClientResponse> createClient(
+      @Parameter(
+              description = "Objeto cliente con los datos necesarios para su creación",
+              required = true)
+          @RequestBody
+          Client clientCreate) {
+    LOGGER.info("Init createClient Client Id: {}", clientCreate.getId());
+    return iClientService.saveClient(clientCreate);
+  }
 
-        LOGGER.info("Init getClientById Client Id: {}", id);
+  @Operation(
+      summary = "Actualizar cliente",
+      description = "Actualiza los datos de un cliente existente.")
+  @PutMapping("/client/{id}")
+  public ResponseEntity<Client> updateClient(
+      @Parameter(description = "ID del cliente a actualizar", required = true)
+          @PathVariable(value = "id")
+          String id,
+      @Parameter(description = "Datos del cliente actualizados", required = true) @RequestBody
+          Client client) {
+    LOGGER.info("Init updateClient client controller : {}\n body : {}", client);
+    return iClientService.updateClient(id, client);
+  }
 
-        return iClientService.findByClientId(id);
-    }
+  @Operation(summary = "Eliminar cliente", description = "Elimina un cliente del sistema.")
+  @DeleteMapping("/client/{id}")
+  public ResponseEntity<String> deleteClient(
+      @Parameter(description = "ID del cliente a eliminar", required = true)
+          @PathVariable(value = "id")
+          String id) {
+    LOGGER.info("Init deleteClient client  : {}\n body : {}", id);
+    return iClientService.deleteClient(id);
+  }
 
-    @PostMapping("/client")
-    public ResponseEntity<ClientResponse> createClient(@RequestBody Client clientCreate) {
-
-        LOGGER.info("Init createClient Client Id: {}", clientCreate.getId());
-
-        return iClientService.saveClient(clientCreate);
-
-    }
-
-    @PutMapping("/client/{id}")
-    public ResponseEntity<Client> updateClient(@PathVariable(value = "id") String id,
-                                               @RequestBody Client client) {
-        LOGGER.info("Init updateClient client controller : {}\n body : {}", client);
-
-        return iClientService.updateClient(id,client);
-
-    }
-
-    @DeleteMapping("/client/{id}")
-    public ResponseEntity<String> deleteClient(@PathVariable(value = "id") String id) {
-
-        LOGGER.info("Init deleteClient client  : {}\n body : {}", id);
-
-        return iClientService.deleteClient(id);
-    }
-
-    @GetMapping("/client/listClients")
-    public ResponseEntity<List<Client>> listClients() {
-
-        LOGGER.info("Init listClients controller : {}\n body : {}");
-
-        return iClientService.listClient();
-    }
-
+  @Operation(
+      summary = "Listar clientes",
+      description = "Retorna una lista de todos los clientes registrados.")
+  @GetMapping("/client/listClients")
+  public ResponseEntity<List<Client>> listClients() {
+    LOGGER.info("Init listClients controller : {}\n body : {}");
+    return iClientService.listClient();
+  }
 }
